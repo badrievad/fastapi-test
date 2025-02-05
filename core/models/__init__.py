dfea3db60134c10f7a2ecbebd7f491bc58be0890
badrievad <zzdl4@bk.ru>
2025-02-05 15:23:55 +0300

    created profiles model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -5,10 +5,12 @@ __all__ = (
     "db_helper",
     "User",
     "Post",
+    "Profile",
 )
 
 from .base import Base
 from .db_helper import DbHelper, db_helper
 from .post import Post
 from .product import Product
+from .profile import Profile
 from .user import User
---
+++ core/models/profile.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .mixins import UserRelationMixin
+
+
+class Profile(UserRelationMixin, Base):
+
+    _user_back_populates = "profile"
+    _user_id_unique = True
+
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    last_name: Mapped[str | None] = mapped_column(String(40))
