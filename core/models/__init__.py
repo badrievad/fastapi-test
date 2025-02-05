77bcd341b1dcd54d883bf2a98beb5b32dc8041d8
badrievad <zzdl4@bk.ru>
2025-02-05 14:45:36 +0300

    created post model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,11 @@ __all__ = (
     "DbHelper",
     "db_helper",
     "User",
+    "Post",
 )
 
 from .base import Base
 from .db_helper import DbHelper, db_helper
+from .post import Post
 from .product import Product
 from .user import User
