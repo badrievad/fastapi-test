23872aa5ff6a06dc01221bc15636d52cac3229e3
badrievad <zzdl4@bk.ru>
2025-02-05 15:24:50 +0300

    little formatting

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.product import Product
 
-from ..products.schemas import ProductCreateSchema, ProductUpdateSchema
+from .schemas import ProductCreateSchema, ProductUpdateSchema
 
 
 async def get_products(session: AsyncSession) -> list[Product]:
