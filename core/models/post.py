9b846667d43475c403f2822b4569ff6d56101de8
badrievad <zzdl4@bk.ru>
2025-02-06 13:45:52 +0300

    added str and repr to models

--- core/models/post.py
+++ core/models/post.py
@@ -15,3 +15,9 @@ class Post(UserRelationMixin, Base):
         default="",
         server_default="",
     )
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, title={self.title!r})"
+
+    def __repr__(self):
+        return str(self)
--- core/models/profile.py
+++ core/models/profile.py
@@ -12,3 +12,11 @@ class Profile(UserRelationMixin, Base):
 
     first_name: Mapped[str | None] = mapped_column(String(40))
     last_name: Mapped[str | None] = mapped_column(String(40))
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}(id={self.id}, first_name={self.first_name!r})"
+        )
+
+    def __repr__(self):
+        return str(self)
